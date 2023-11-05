b841a49b0296c1ff1133f61cd84dda3a5c0f5ca2
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-11-05 22:51:49 +0600

    Added delete api

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors')
 const port = 5000;
 
-
+// Middleware ---------------------------------------
 app.use(cors());
 app.use(express.json())
 
-// Mongodb connection
-
+// Mongodb connection--------------------------------
 const uri = "mongodb+srv://gallery:FIjIKASFa6H448Uy@cluster0.icnhbae.mongodb.net/?retryWrites=true&w=majority";
 
-// mongodb client
+// mongodb client ------------------------------------
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -21,14 +20,14 @@ const client = new MongoClient(uri, {
     }
 });
 
-// client connection function
+// client connection function -----------------------
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+        // Connect the client to the server	(optional starting in v4.7) ----------------------------
         await client.connect();
         const imageUrlConnection = client.db("image-gallery").collection("imageUrl")
 
-        //Add image in gallery
+        //Add image in gallery -----------------------
         app.post('/addImageUrl', async (req, res) => {
             const imgeUrl = req.body;
             const result = await imageUrlConnection.insertOne(imgeUrl);
@@ -36,11 +35,25 @@ async function run() {
             res.send(result)
         })
 
-        //Get image
-        app.get('/images',async(req,res)=>{
+        //Get image----------------------------------
+        app.get('/images', async (req, res) => {
+
             const cursor = imageUrlConnection.find({});
             const allGalleryImage = await cursor.toArray();
             res.send(allGalleryImage);
+        })
+
+        // Delete images ------------------------------
+        app.delete('/deleteImages', async (req, res) => {
+            const data = req.body;
+            console.log(data)
+            let result;
+            for (const x of data) {
+                console.log(x)
+                const filter = { _id: new ObjectId(x) }
+                result = await imageUrlConnection.deleteMany(filter)
+            }
+            res.send(result)
         })
 
 
@@ -53,9 +66,7 @@ run();
 
 
 
-
-
-// server listening
+// server listening -------------------------------
 app.listen(port, () => {
     console.log("Server is running")
 })
